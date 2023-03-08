d297c2652dfc08248db690aeb30c26b37f38383d
Anderson Sales <andersonsales.mg@gmail.com>
2023-03-08 18:11:29 -0300

    Create Project Files

---
+++ Layout&Sizes/Layout_SizesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Layout_SizesApp.swift
+//  Layout&Sizes
+//
+//  Created by Anderson Sales on 08/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Layout_SizesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
